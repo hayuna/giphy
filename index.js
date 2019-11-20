691aedae1a917a199268a61c417a02a344bbf1fd
mras2303 <marek.kowalonek@gmail.com>
2019-11-20 23:39:47 +0100

    Update and rename main.ts to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core')
+const { context } = require('@actions/github')
+
+(async function run() {
+    try {
+        core.debug('message sent')
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+})()
